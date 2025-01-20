45f3e535cc0e26c78c12813549c0a6b159045c9e
lecture <ksw617@gmail.com>
2025-01-20 20:10:23 +0900

    Update cMain.cpp

--- Day06/cMain.cpp
+++ Day06/cMain.cpp
@@ -1,14 +1,56 @@
 #include <stdio.h>
 #include <Windows.h>
 
+enum SCENE
+{
+	LOGO,
+	MENU,
+	STAGE,
+};
+
+void Logo()
+{
+	printf("Logo\n");
+	if (GetAsyncKeyState(VK_RETURN))
+	{
+	   // //scene을 MENU로 넘겨주세요.
+	}
+}
+
+void Menu()
+{
+	printf("Menu\n");
+}
+
+void Stage()
+{
+	printf("Stage\n");
+}
+
 int main()
 {
+	SCENE scene = LOGO;
+
+
 	while (true)
 	{
-		system("cls");
-		printf("Hello world");
+		//system("cls");
+		switch (scene)
+		{
+		case 0:
+			Logo();
+			break;
+		case 1:
+			Menu();
+			break;
+		case 2:
+			Stage();
+			break;
+		default:
+			break;
+		}
 
-		Sleep(50);
+		Sleep(1000);
 	}
 	return 0;
 }
